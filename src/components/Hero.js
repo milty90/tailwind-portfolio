b659e5194ccb14bd4efb0ca89fa92541bd37c1f4
milty90 <156122688+milty90@users.noreply.github.com>
2026-02-08 21:45:23 +0100

    fix: social media icons with hover effects

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,9 @@ export default function Hero() {
         <p class="text-4xl md:text-6xl mb-4 md:mb-8">Ihr neuer <span class="font-bold bg-linear-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent"><br>Frontend Developer.</span></p>
         <p class="text-l text-gray-400 mb-8 md:text-xl md:mb-10">Mein Name ist Milan und ich bin Softwareentwickler mit Schwerpunkt auf den Bereich Frontend Web Development. Ich liebe es neue Dinge zu lernen und Ideen in die Realität zu bringen.</p>
         <div class="flex space-x-4  rounded-lg mb-10">
-        <i class="fa-brands fa-linkedin text-3xl text-gray-400 hover:text-gray-100 lg:text-4xl"></i>
-        <i class="fa-brands fa-github text-3xl text-gray-400 hover:text-gray-100 lg:text-4xl"></i>
-        <i class="fa-brands fa-instagram text-3xl text-gray-400 hover:text-gray-100 lg:text-4xl"></i>
+        <i class="fa-brands fa-linkedin text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
+        <i class="fa-brands fa-github text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
+        <i class="fa-brands fa-instagram text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
         </div>
         <a href="#projects" class="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg transition">Projecte ansehen  ↓</a>
       </div>
