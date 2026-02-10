08e4b3c29290a7e43094a22ab1a12f5a2a36a647
milty90 <156122688+milty90@users.noreply.github.com>
2026-02-10 23:44:19 +0100

    refactor: update Hero and Projects components

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,17 @@
 export default function Hero() {
   return `
-    <section class=" relative h-full mx-auto grid lg:grid-cols-2 gap-10 pt-0 max-w-300 bg-gray-900 text-white">
+    <section class="h-full mx-auto grid lg:grid-cols-2 gap-10 pt-0 max-w-300 bg-gray-900 text-white">
       <div class="text-left flex flex-col items-start md:flex-col md:px-25 lg:pl-10 lg:pr-0">
         <img src="./avatar.png" alt="profile picture" class="w-20 h-20 rounded-full border-3 border-gray-400 object-cover ml-2 mb-6 md:mb-10 md:w-30 md:h-30 lg:hidden">
         <p class="text-xl md:text mb-4 pl-1 lg:text-2xl  ">Hi, ich bin <span class="font-bold text-gray-300">Milan.</span></p>
         <p class="text-4xl md:text-6xl mb-4 md:mb-8">Ihr neuer <span class="font-bold bg-linear-to-r from-blue-600 via-purple-500 to-pink-500 bg-clip-text text-transparent"><br>Frontend Developer.</span></p>
         <p class="text-l text-gray-400 mb-8 md:text-xl md:mb-10">Mein Name ist Milan und ich bin Softwareentwickler mit Schwerpunkt auf den Bereich Frontend Web Development. Ich liebe es neue Dinge zu lernen und Ideen in die Realität zu bringen.</p>
         <div class="flex space-x-6  rounded-lg mb-10">
-        <i class="fa-brands fa-linkedin text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
-        <i class="fa-brands fa-github text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
-        <i class="fa-solid fa-envelope text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-4xl"></i>
+        <i class="fa-brands fa-linkedin text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-3xl"></i>
+        <i class="fa-brands fa-github text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-3xl"></i>
+        <i class="fa-solid fa-envelope text-3xl text-gray-400 hover:text-gray-100 hover:scale-120 transition-all duration-75 lg:text-3xl"></i>
         </div>
-        <a href="#" class="ml-1 text-xl bg-blue-600 hover:bg-linear-to-r hover:scale-110 hover:translate-x-2 duration-200  from-blue-600 via-purple-500 to-pink-500 px-6 py-3 rounded-lg transition">Projecte ansehen  ↓</a>
+        <a href="#" class="ml-1 text-lg bg-blue-600 hover:bg-linear-to-r hover:scale-110 hover:translate-x-2 duration-200  from-blue-600 via-purple-500 to-pink-500 px-5 py-2.5 rounded-lg transition">Projekte ansehen <i class="fa-solid fa-arrow-down ml-2.5 mb-1.5 text-sm"></i></a>
       </div>
       <div class="flex items-center justify-center md:relative">
        <img src="./techs.png" alt="techs" class="object-cover sm:max-w-110 lg:absolute lg:inset-0 xl:max-w-140">
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,32 +4,48 @@ export default function Projects() {
       <h2 class=" text-4xl font-bold text-white mt-20 mb-5">Meine Projekte</h2>
       <p class="text-gray-400 text-xl mb-10">lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="bg-gray-800 max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <div class="bg-gray-800 h-full max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col">
           <img src="./project_1.jpg" alt="Pizzeria Mamma Mia" class="w-full h-70 object-cover">
-          <div class="p-6">
+          <div class="flex flex-col flex-1 p-6">
             <h3 class="text-2xl font-semibold text-white mb-4">Pizzeria Mamma Mia</h3>
-            <a href="https://the-restaurant-project.onrender.com" target="_blank" class="inline-block text-blue-500 mb-3 hover:underline">restaurant-project.onrender.com</a>
+            <a href="https://the-restaurant-project.onrender.com" target="_blank" class="inline-block text-blue-500 mb-3 font-medium hover:underline">restaurant-project.onrender.com <i class="fa-solid fa-up-right-from-square"></i> </a>
             <p class="text-gray-400 mb-4">lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <a href="#" class="inline-block bg-blue-500 mt-2 p-2 rounded-md text-amber-50 ">Code ansehen</a>
+            <div class="gap-1 flex-wrap flex mb-2">
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">HTML</span>
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">CSS</span>
+            </div>
+            <a href="https://github.com/milty90/The-Restaurant-Project" target="_blank" class="w-fit bg-blue-500 mt-auto p-2 rounded-md font-medium text-amber-50 "><i class="fa-solid fa-code"></i>  Code ansehen</a>
           </div>    
         </div>
-        <div class="bg-gray-800 max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">   
+        <div class="bg-gray-800 h-full max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col">   
             <img src="./project_2.jpg" alt="Taschenrechner App" class="w-full h-70 object-cover">
-            <div class="p-6">
+          <div class="flex flex-col flex-1 p-6">
               <h3 class="text-2xl font-semibold text-white mb-4">Taschenrechner App</h3>
-              <a href="https://taschenrechner-m0ix.onrender.com" target="_blank" class="inline-block text-blue-500 mb-3 hover:underline">taschenrechner.onrender.com</a>
-              <p class="text-gray-400 mb-4">lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <a href="#" class="inline-block bg-blue-500 mt-2 p-2 rounded-md text-amber-50 ">Code ansehen</a>
-
-            </div>
+              <a href="https://taschenrechner-m0ix.onrender.com" target="_blank" class="inline-block text-blue-500 mb-3 font-medium hover:underline">taschenrechner.onrender.com <i class="fa-solid fa-up-right-from-square"></i> </a>
+              <p class="text-gray-400 mb-4"> lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <div class="gap-1 flex-wrap flex mb-2">
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">HTML</span> 
+                <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">CSS</span>
+                <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">JavaScript</span>
+              </div>  
+              <a href="https://github.com/milty90/Taschenrechner" target="_blank" class="w-fit bg-blue-500 mt-auto p-2 rounded-md font-medium text-amber-50 "><i class="fa-solid fa-code"></i>  Code ansehen</a>
+              </div>
         </div>
-        <div class="bg-gray-800 h-full max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
+        <div class="bg-gray-800 h-full max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col">
             <img src="./project_3.jpg" alt="Witze App" class="w-full h-70 object-cover">
-            <div class="h-full p-6">
+          <div class="flex flex-col flex-1 p-6">
               <h3 class="text-2xl font-semibold text-white mb-4">Witze App</h3>
-              <a href="https://witze-app.onrender.com/" target="_blank" class="inline-block text-blue-500 mb-3 hover:underline">witze-app.onrender.com</a>
+              <a href="https://witze-app.onrender.com/" target="_blank" class="inline-block text-blue-500 mb-3 font-medium hover:underline">witze-app.onrender.com <i class="fa-solid fa-up-right-from-square"></i> </a>
               <p class="text-gray-400 mb-4">lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              <a href="#" class="inline-block bg-blue-500 mt-2 p-2 rounded-md text-amber-50 ">Code ansehen</a>
+              <div class="flex flex-wrap gap-2 pb-2 mb-auto">
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">HTML</span>
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">JavaScript</span>   
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">SCSS</span>   
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">BEM</span> 
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">Vite</span>   
+              <span class="text-amber-50 font-medium bg-gray-900 px-3.5 py-0.5 mb-1 rounded-xl text-sm">AJAX</span>     
+              </div>
+              <a href="https://github.com/milty90/witze-app" target="_blank" class="w-fit bg-blue-500 mt-4 p-2 rounded-md font-medium text-amber-50 "><i class="fa-solid fa-code"></i>  Code ansehen</a>
             </div>
         </div>
       </div>
